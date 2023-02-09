65f12b7bb1e3bef676cd7ac8441d67b47f2b0a8e
zar <aka.zarj@gmail.com>
2023-02-10 01:54:18 +0600

    first

---
+++ src/test/java/com/madgods/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madgods.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
